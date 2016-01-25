4f5be63ea0c9b23b0172bef23ef37d233cd15e74
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-01-25 21:46:38 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
+
+var bodyParser = require("body-parser");
+
+var pg = require("pg");
+var conString = process.env.DATABASE_URL || "postgres://localhost:5432/jonathan";
+
+app.use(bodyParser.json());
+
+app.get("/search",query_creater,db_search,end);
+
+function query_creater(req,res,next){
+  console.log(req.query.subject);
+  var search_query = 'SELECT column FROM ' + req.query.subject + 'ORDER BY RANDOM() LIMIT 1';
+  next(search_query);
+}
+
+function end(req,res){
+  res.end;
+}
+
+function db_search(qry,req,res,next){
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(qry,function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      res.json(result.rows);
+    });
+  });
+  next();
+}
