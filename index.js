526079567e7ef8f04dc44de16659dc5babe64490
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-02-22 23:01:54 +0100

    Not working but bettter save

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ var port = process.env.PORT || 3000;
 //var router = express.Router();
 var server = app.listen(port);
 
+var id = [
+  {"name" : "deutsch" , "id" : 0},
+  {"name" : "mathe", "id" : 0},
+  {"name" : "englisch", "id" : 0},
+  {"name" : "franzoesisch", "id" : 0},
+  {"name" : "spanisch", "id" : 0},
+  {"name" : "geschichte", "id" : 0},
+  {"name" : "erdkunde", "id" : 0},
+  {"name" : "sport", "id" : 0},
+  {"name" : "chemie", "id" : 0},
+  {"name" : "physik", "id" : 0},
+  {"name" : "biologie", "id" : 0},
+  {"name" : "bildendekunst", "id" : 0},
+  {"name" : "gemeinschaftskunde", "id" : 0},
+  {"name" : "musik", "id" : 0}
+];
+
+var names = {
+  "deutsch" : 0,
+  "mathe" : 1,
+  "englisch" : 2,
+  "franzoesisch" : 3,
+  "spanisch" : 4,
+  "geschichte" : 5,
+  "erdkunde" : 6,
+  "sport" : 7,
+  "chemie" : 8,
+  "physik" : 9,
+  "biologie" : 10,
+  "bildendekunst" : 11,
+  "gemeinschaftskunde" : 12,
+  "musik" : 13
+};
+
 var bodyParser = require("body-parser");
 
 var pg = require("pg");
@@ -11,17 +45,63 @@ var conString = process.env.DATABASE_URL || "postgres://localhost:5432/jonathan"
 
 app.use(bodyParser.json());
 
-app.get("/search",query_creater,db_search,end);
+
+function set_id () {
+  for(var i = 0;i <= id.length-1;i++){
+    var table_size_query = "SELECT MAX(id) FROM " + id[i].name;
+    var fake_req = {"path" : "/table_size_query", "i" : i};
+    var fake_res = {"id" : -1};
+    db_search(table_size_query,fake_req,fake_res,function(req,res){
+        id[req.i].id = res.id;
+        console.log(res.id);
+      }
+    );
+  }
+}
+
+set_id();
+
+app.get("/search",search_query_creater,db_search,end);
+
+app.post("/new-question",new_question_query_creater,db_search,end);
 
 // Creates a query and gives it to db_search
 
-function query_creater(req,res,next){
+function search_query_creater(req,res,next){
   var search_query = 'SELECT frage, antwort, falsch_1, falsch_2, falsch_3 FROM ' + req.query.subject + ' ORDER BY RANDOM() LIMIT 1';
+
   next(search_query);
 }
 
-// Ends response
+/*function table_size_query_creater(req,res,next){
+  var table_size_query = "SELECT MAX(id) FROM " + req.body.subject;
+  console.log("1: " + table_size_query);
+  console.log("");
+  console.log("");
+  console.log("");
+  next(table_size_query);
+}*/
 
+function new_question_query_creater(req,res,next){
+  var curent_id;
+  console.log(id.length);
+  for(var i = 0; i <= id.length; i++){
+    console.log(i);
+    if(id[i].name == req.body.subject){
+      id[i].id++;
+      curent_id = id[i].id;
+      console.log("worked");
+    }
+  }
+  //console.log("left loop");
+  //console.log("someting");
+  //var new_question_query = 'INSERT INTO ' + req.body.subject + '(id, frage, antwort, falsch_1, falsch_2, falsch_3, creater, kategorie) VALUES(' + curent_id + ', ';
+  //new_question_query = new_question_query + req.body.frage + ', ' + req.body.antwort + ', ' + req.body.falsch_1 + ', ' + req.body.falsch_2 + ', ' + req.body.falsch_3 + ', ' + req.body.creater + ', ' + req.body.kategorie + ')';
+  //console.log(new_question_query);
+  //console.log("someting");
+  //next();
+  //next(new_question_query);
+}
 
 function end(req,res){
   res.end;
@@ -31,23 +111,39 @@ function end(req,res){
    returned row as a JSON response */
 
 function db_search(qry,req,res,next){
-  pg.connect(conString, function(err, client, done) {
+  console.log("sommmmmmmmeting");
+
+  console.log(qry);
+  pg.connect(conString, function connecter_function(err, client, done) {
     if(err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query(qry,function(err, result) {
+    client.query(qry,function result_switch(err, result) {
       done();
       if(err) {
-        return console.error('error running query', err);
+        return console.error('error running query', err,qry);
       }
-      console.log(result.rows[0]);
-
+      //console.log(result.rows[0]);
+      console.log("database query");
+      console.log(qry);
       switch (req.path) {
 
         case "/search":
 
           res.json(result.rows[0]);
           next();
+          break;
+
+        case "/new-question":
+          console.log("somettttttttttting");
+
+          next();
+          break;
+
+        case "/table_size_query":
+
+          res.id = result.rows[0].max
+          next(req,res);
           break;
 
         default:
