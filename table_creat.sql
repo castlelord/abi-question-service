cf65eb93f7ced79d1f8d51bb1cd7dd6ba893a28a
Jonathan Burggraf <jonathan.burggraf@gmail.com>
2016-02-22 23:00:14 +0100

    Add Sql commands

---
+++ table_creat.sql
@@ -0,0 +1,160 @@
+CREATE TABLE deutsch
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE mathe
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE englisch
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE franzoesisch
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE spanisch
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE geschichte
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE erdkunde
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE sport
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE physik
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE biologie
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE bildende-kunst
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE gemeinschaftskunde
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+CREATE TABLE musik
+(
+  id int,
+  frage text,
+  antwort text,
+  falsch_1 text,
+  falsch_2 text,
+  falsch_3 text,
+  creater text,
+  kategorie int
+);
+
+
+
+
+
+INSERT INTO deutsch VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO mathe VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO englisch VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO franzoesisch VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO spanisch VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO erdkunde VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO sport VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO physik VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO biologie VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO bildende-kunst VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO gemeinschaftskunde VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
+INSERT INTO musik VALUES(0,'frage','antwort','falsch','falsch','falsch','jonathan burggraf',0);
